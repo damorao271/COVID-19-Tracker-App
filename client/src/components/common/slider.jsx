bba4a133487c9424cede1016ff8fd6ac2e769905
Daniel Morao <13-10932@usb.ve>
2020-05-30 00:01:29 -0400

    Selection of Sliders

--- client/src/components/common/slider.jsx
+++ client/src/components/common/slider.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
 
@@ -7,32 +7,69 @@ class SliderComponent extends Component {
   render() {
     const { counter, fechas } = this.props;
     var fecha = new Date(fechas[counter]);
-
+    const maxLength = fechas.length;
     var dia = fecha.toLocaleString("en-US", {
       day: "numeric",
       month: "numeric",
       year: "numeric",
     });
 
-    return (
-      <div className="slider-contaier">
-        <h3>Slider</h3>
-        {dia}
+    const PrettoSlider = withStyles({
+      root: {
+        color: "#52af77",
+        height: 8,
+      },
+      thumb: {
+        height: 24,
+        width: 24,
+        backgroundColor: "#fff",
+        border: "2px solid currentColor",
+        marginTop: -8,
+        marginLeft: -12,
+        "&:focus, &:hover, &$active": {
+          boxShadow: "inherit",
+        },
+      },
+      active: {},
+      valueLabel: {
+        left: "calc(-50% + 4px)",
+      },
+      track: {
+        height: 8,
+        borderRadius: 4,
+      },
+      rail: {
+        height: 8,
+        borderRadius: 4,
+      },
+    })(Slider);
 
-        <Typography id="discrete-slider" gutterBottom>
-          Temperature
-        </Typography>
-        <Slider
-          defaultValue={30}
-          getAriaValueText={dia}
-          // aria-labelledby="discrete-slider"
-          valueLabelDisplay="auto"
-          step={10}
-          marks
-          min={10}
-          max={110}
-        />
-      </div>
+    return (
+      <React.Fragment>
+        <div className="slider-contaier">
+          <Typography id="discrete-slider" gutterBottom>
+            Date: {dia}
+          </Typography>
+          <div>
+            <Slider
+              // defaultValue={counter}
+              // getAriaValueText={counter}
+              color="red"
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={0}
+              max={maxLength}
+            />
+            <PrettoSlider
+              valueLabelDisplay="auto"
+              aria-label="pretto slider"
+              defaultValue={20}
+            />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
